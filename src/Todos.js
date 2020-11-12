a7ef3368cff1918205500c46999c992667dbde8d
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-11 17:44:36 -0800

    building out todo page refactoring

--- src/Todos.js
+++ src/Todos.js
@@ -4,6 +4,9 @@ import request from 'superagent';
 export default class Todos extends Component {
     state = {
         todos: [],
+        todo: '',
+        isCompleted: false,
+        loading: false
     }    
 
     componentDidMount = async () => {
@@ -11,6 +14,38 @@ export default class Todos extends Component {
         .set('Authorization', this.props.token)
         this.setState({todos: response.body})
     }
+
+    fetchTodos = async () => {
+        const { token } = this.props;
+
+        await this.setState({ loading: true });
+        const response = await request.
+        get('https://secret-chamber-28719.herokuapp.com/api/todos')
+        .set('Authorization', token)
+
+        await this.setState({ todos: response.body, loading: false })
+    }
+
+    handleSubmit =async (e) => {
+        const { todo, isCompleted } = this.state;
+        const { token } = this.props;
+
+        e.preventDefault();
+
+        const newTodo = {
+            todo: todo,
+            is_completed: isCompleted
+        };
+
+        await this.setState({loading: true});
+
+        await request.post('https://secret-chamber-28719.herokuapp.com/api/todos')
+        .send(newTodo)
+        .set('Authorization', token);
+
+        await this.fetchTodos();
+    }
+
     render() {
         return (
             <div>
